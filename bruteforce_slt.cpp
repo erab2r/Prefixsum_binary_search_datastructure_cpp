94612915d1c9fbc379603bacb1fad854ee6a78fd
erab2r <shkera213@gmail.com>
2025-06-22 13:59:39 +0600

    ghgfh

--- bruteforce_slt.cpp
+++ bruteforce_slt.cpp
@@ -4,16 +4,24 @@ int main(){
     int n,q;
     cin >> n >> q;
     vector<int> a(n);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<=n;i++){
         cin >> a[i];
+    }
+    vector<int>pre(n+1);
+    pre[1] = a[1];
+    for(int i=1;i<=n;i++){
+        pre[i] = pre[i-1] + a[i];
     }
     while(q--){
         int l,r;
         cin >> l >> r;
         int sum = 0;
-        for(int i = l;i<r;i++){
-             sum +=a[i];
+        if(l==1){
+            sum =pre[r];
         }
+        else{
+            sum =pre[r] - pre[l-1];
+        }    
         cout << sum << endl;
        
     }
